54b6d8a680da6240bd5bc87ebfd80beadea3f92f
devspectre <devspectre@yandex.com>
2020-05-27 16:31:46 +0300

    get maximum palindromic number

--- new.py
+++ new.py
@@ -5,6 +5,10 @@ def is_palindromic(number: int) -> bool:
 	return False
 
 if __name__ == '__main__':
+
+	max_value = 0
 	for i in range(100000, 1000000):
-		if is_palindromic(i + 1):
-			print(i + 1)
+		if is_palindromic(i):
+			max_value = i
+
+	print(max_value)
